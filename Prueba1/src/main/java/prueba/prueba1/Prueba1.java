019eb758e67c8387e9e5da69ee5e1a30673cb5e6
RaulCas7 <119075870+RaulCas7@users.noreply.github.com>
2023-01-11 11:47:13 +0100

    Conflicto

--- Prueba1/src/main/java/prueba/prueba1/Prueba1.java
+++ Prueba1/src/main/java/prueba/prueba1/Prueba1.java
@@ -13,6 +13,8 @@ public class Prueba1 {
 
     public static void main(String[] args) {
         System.out.println("Me llamo Raul");
+        
         System.out.println("Modificacion Remota")
+            
     }
 }
